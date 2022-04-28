2ff350a824d03a04c82a5291776eb13c3ab7f150
RudraDeshmukh <99274112+RudraDeshmukh@users.noreply.github.com>
2022-04-28 17:39:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function preload(){
+nosex=0;
+nosey=0;
+var clown_nose_img;
 
+function preload(){
+    clown_nose_img=loadImage("https://i.postimg.cc/JnN6Zy4Z/clown-nose.png");
 }
 function setup(){
     canvas = createCanvas(300,300)
@@ -16,6 +20,8 @@ function gotResults(results){
         console.log(results)
         console.log("Nose x = "+ results[0].pose.nose.x)
         console.log("Nose y = "+ results[0].pose.nose.y)
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
     }
 }
 
@@ -25,6 +31,13 @@ function modelloaded(){
 
 function draw(){
 image(video,0,0,300,300)
+fill(255,0,0);
+stroke(255,0,0);
+circle(nosex,nosey,20)
+
+image(clown_nose_img,nosex-15,nosey-15,30,30)
+
+
 }
 
 function take_snap(){
